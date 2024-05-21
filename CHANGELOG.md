464bb982d70413483fc85ef5654ebc2312cf8006
Andrew Kane <andrew@ankane.org>
2024-05-21 10:33:02 -0400

    Updated ONNX Runtime to 1.18.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1 (unreleased)
+
+- Updated ONNX Runtime to 1.18.0
+
 ## 0.2.0 (2024-02-07)
 
 - Updated ONNX Runtime to 1.17.0
--- src/Vendor.php
+++ src/Vendor.php
@@ -4,36 +4,36 @@ namespace OnnxRuntime;
 
 class Vendor
 {
-    public const VERSION = '1.17.0';
+    public const VERSION = '1.18.0';
 
     public const PLATFORMS = [
         'x86_64-linux' => [
             'file' => 'onnxruntime-linux-x64-{{version}}',
-            'checksum' => 'efc344d54d1969446ff5d3e55b54e205c6579c06333ecf1d34a04215eefae7c6',
+            'checksum' => 'fa4d11b3fa1b2bf1c3b2efa8f958634bc34edc95e351ac2a0408c6ad5c5504f0',
             'lib' => 'libonnxruntime.so.{{version}}',
             'ext' => 'tgz'
         ],
         'aarch64-linux' => [
             'file' => 'onnxruntime-linux-aarch64-{{version}}',
-            'checksum' => 'ee5069252f549ef94759b6b60bdf10b2dc2cd71d064a7045dd66a052f956a68b',
+            'checksum' => 'c278ca7ce725d2b26cf1ec62c93affaec4145a9a3d7721fb5d1af5497136ca76',
             'lib' => 'libonnxruntime.so.{{version}}',
             'ext' => 'tgz'
         ],
         'x86_64-darwin' => [
             'file' => 'onnxruntime-osx-x86_64-{{version}}',
-            'checksum' => 'b87b2febef24e5645e13859d176e76473124325a0b1526baf7f68b4aa1eb1b49',
+            'checksum' => '3af96893675b295e5e0eb886f470de585089f92f9950158d042fbc02b44ed101',
             'lib' => 'libonnxruntime.{{version}}.dylib',
             'ext' => 'tgz'
         ],
         'arm64-darwin' => [
             'file' => 'onnxruntime-osx-arm64-{{version}}',
-            'checksum' => 'f72a2bcca40e2650756c6b96c69ef031236aaab1b98673e744da4eef0c4bddbd',
+            'checksum' => 'c5ff520d2913e3360670979ca4fe43717fc3aa0c0c367a75fbb6f2f15c0cb48d',
             'lib' => 'libonnxruntime.{{version}}.dylib',
             'ext' => 'tgz'
         ],
         'x64-windows' => [
             'file' => 'onnxruntime-win-x64-{{version}}',
-            'checksum' => 'b0436634108c001e2284cb685646047a7b088715b64c05e39ee8a1a8930776a9',
+            'checksum' => 'a91af21ca8f9bdfa5a1aac3fdd0591384b4e2866d41612925f1758d5522829e7',
             'lib' => 'onnxruntime.dll',
             'ext' => 'zip'
         ]
