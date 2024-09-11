a195ba4c0182587d336bc1d8520d18224ec8bb4c
Andrew Kane <andrew@ankane.org>
2024-09-11 01:26:57 -0700

    Added test for dataPtr method [skip ci]

--- tests/OrtValueTest.php
+++ tests/OrtValueTest.php
@@ -13,6 +13,9 @@ final class OrtValueTest extends TestCase
         $this->assertEquals([1, 2], $value->shape());
         $this->assertEquals('cpu', $value->deviceName());
         $this->assertEqualsWithDelta([5.8, 2.8], $value->toObject()[0], 0.00001);
+        $dataPtr = $value->dataPtr();
+        $this->assertEqualsWithDelta(5.8, $dataPtr[0], 0.00001);
+        $this->assertEqualsWithDelta(2.8, $dataPtr[1], 0.00001);
     }
 
     public function testOrtvalueFromShapeAndType()
